6b841c83e871d9ed2e5cc6b49f0bb857e6e1f0a2
birdday <22bday@gmail.com>
2021-06-21 19:04:43 -0400

    Updated parameter files for TraPPE-General, includng adding a commented version of parameter files for ease of updating / preventing duplicate parameters. Added corresponding python script to create non-commented version of the files, as needed for RASPA.

---
+++ python_scripts/fix_comments_force_field_mixing_rules.py
@@ -0,0 +1,42 @@
+import csv
+import sys
+
+file = '/Users/brian_day/Github-Repos/RASPA_Parameters/forcefield/TraPPE-General/force_field_mixing_rules_COMMENTS.def'
+file_out = '/Users/brian_day/Github-Repos/RASPA_Parameters/forcefield/TraPPE-General/force_field_mixing_rules.def'
+
+# Load file as text and strip whitespace
+file_as_text = []
+with open(file, newline='') as csvfile:
+     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+     for row in spamreader:
+         row_new = []
+         for entry in row:
+             if entry != '':
+                 row_new.extend([entry])
+         file_as_text.extend([row_new])
+csvfile.close()
+
+# Remove empty rows and non-necessary comment rows
+file_as_text_new =[]
+for i in range(len(file_as_text)):
+    row = file_as_text[i]
+    # Removes Empty Rows
+    if row == []:
+        continue
+    # Removes unnecessary comments
+    elif row[0] == '#' and i > 6 and i < len(file_as_text)-2:
+        continue
+    # Removes bracketed charges which are specified in pseudo-atoms file
+    elif len(row) > 2 and i > 6 and i < len(file_as_text)-2:
+        if row[1] == 'lennard-jones':
+            file_as_text_new.extend([row[0:4]])
+        elif row[1] == 'none':
+            file_as_text_new.extend([row[0:2]])
+    else:
+        file_as_text_new.extend([row])
+
+
+with open(file_out, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    writer.writerows(file_as_text_new)
+csvfile.close()
